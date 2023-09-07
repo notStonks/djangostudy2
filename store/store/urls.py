4dd2b43397ab5da1ac488cf0f7d441f88410f308
notStonks <kuzewich.a@gmail.com>
2023-09-07 19:10:59 +0300

    небольшие изменения для работы на удаленном сервере

--- store/store/urls.py
+++ store/store/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+static_urlpatterns = [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
