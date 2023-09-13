98ad2ee6d833f7ea64f38b4541826d7df7d673a6
notStonks <kuzewich.a@gmail.com>
2023-09-13 18:36:41 +0300

    установлен drf, создано приложения для дальнейшей работы

--- store/store/settings.py
+++ store/store/settings.py
@@ -56,10 +56,12 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
     'debug_toolbar',
     'django_extensions',
+    'rest_framework',
 
     'products',
     'orders',
     'users',
+    'api',
 
 ]
 
